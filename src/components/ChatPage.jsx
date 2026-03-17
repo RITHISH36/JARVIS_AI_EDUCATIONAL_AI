d4d5b19a5ba879fd93ca143cbbe2b2413c1d3e5c
rithish <rith172203k@gmail.com>
2026-03-17 11:05:12 +0530

    ChatPage-dev2

--- src/components/ChatPage.jsx
+++ src/components/ChatPage.jsx
@@ -1,15 +1,32 @@
 import { useRef, useState } from "react";
-import { LuArrowUp, LuPanelRight, LuPlus, LuSquarePen } from "react-icons/lu"
+import { LuArrowUp, LuPanelLeft, LuPanelRight, LuPlus, LuSquarePen } from "react-icons/lu"
 import gsap from "gsap"
 import { useGSAP } from "@gsap/react";
+import { Icon } from "@iconify/react";
 gsap.registerPlugin(useGSAP)
+const DUMMY_OUTPUT = [{
+  output: "this is a out from gemini."
+}]
 const ChatPage = () => {
   const textarearef = useRef(null)
   const [Expanded, setExpanded] = useState(false)
   const [UserInput, setUserInput] = useState([])
+  const [UserOutput, setUserOutput] = useState(DUMMY_OUTPUT)
+  const [OptionExpand, setOptionExpand] = useState(false)
   const GsapInput = useRef(null);
   const Gsapchat = useRef(null)
+  const GsapOutput = useRef(null)
+  const [SettingsOpen, setSettingsOpen] = useState(false);
+  const handleOption = () => {
+    setOptionExpand(!OptionExpand)
+  }
+  console.log(OptionExpand);
+  const handelSettings = (el) => {
+    el.preventDefault();
+    setSettingsOpen(!SettingsOpen);
+  }
   const handleSubmit = (el) => {
+    el.preventDefault();
     gsap.from(GsapInput.current, {
       opacity: 0,
       y: 25,
@@ -17,7 +34,12 @@ const ChatPage = () => {
       delay: 0.1,
       ease: "power2.out"
     });
-    el.preventDefault();
+    gsap.from(GsapOutput.current, {
+      y: 20,
+      duration: 0.4,
+      delay: 0.1,
+      ease: "power2.inOut"
+    })
     const data = textarearef.current.value;
     setUserInput((prev) => [...prev, data])
     console.log(UserInput)
@@ -52,13 +74,63 @@ const ChatPage = () => {
   return (
     <>
       <div className="ChatPage-Option">
-        <div className="Option-Wapper">
+        <div className={`Option-Wapper ${OptionExpand ? "Option-Open" : ""}`}>
           <div className="window-button">
-            <LuPanelRight size={20} />
+            <LuPanelRight size={20} onClick={handleOption} />
           </div>
           <div className="NewPage-Button">
             <LuSquarePen size={20} />
+            <span className={OptionExpand ? "Display" : "NotDisplay"}>New Page</span>
           </div>
+          {OptionExpand ? (<>
+            <div className="Super-Option">
+              <div className="My-stuff">
+                <Icon icon="lsicon:data-outline" width={24} />
+                <span>My Stuff</span>
+              </div>
+              <div className="Smart-view">
+                <Icon icon="material-symbols:youtube-video-outline-rounded" width={24} />
+                <span>Smart View</span>
+              </div>
+              <div className="Insight-Scale">
+                <Icon icon="iconamoon:scanner-bold" width={24} />
+                <span>Insight Scale</span>
+              </div>
+              <div className="Knowledge-Tree">
+                <Icon icon="tdesign:map-connection" width={24} />
+                <span>Knowledge Tree</span>
+              </div>
+              <div className="Daily-Sparks">
+                <Icon icon="material-symbols:quiz-outline" width={24} />
+                <span>Daily Sparks</span>
+              </div>
+              <div className="Insight-Board">
+                <Icon icon="akar-icons:dashboard" width={24} />
+                <span>Insight Board</span>
+              </div>
+            </div>
+            <hr className="Horizontal-Line" />
+            <div className="User-Account-Option">
+              {SettingsOpen ? (<div className="Settings-Option">
+                <ul style={{ listStyle: "none" }}>
+                  <li className="User-Name">Name<div className="Email">email</div></li>
+                  <hr className="Horizontal-Settings" />
+                  <li className="User-Customization">AI Control Panel</li>
+                  <li className="User-Settings">Control Center</li>
+                  <hr className="Horizontal-Settings" />
+                  <li className="Help-Centre">Help Center</li>
+                  <li className="Log-out">Power Down</li>
+                </ul>
+              </div>) : null}
+              <div className="User-Profile" onClick={handelSettings}>
+                <img src="src/assets/Dummy_profile.jpg" alt="User-Profile" />
+              </div>
+              <div className="Claim-offer">
+                <button>Claim Offers</button>
+              </div>
+            </div>
+          </>
+          ) : null}
           <div className="Subcription-button">
             <img src="src/assets/avatar.svg" alt="Subcription" width={30} height={30} />
           </div>
@@ -68,14 +140,24 @@ const ChatPage = () => {
       <div className="ChatPage">
         <div className="ChatPage-Input" ref={GsapInput}>
           {UserInput.map((el, index) => {
-            return <p key={index} className="UserInput">{el}</p>
+            return el ? (
+              <p key={index} className="UserInput">{el}</p>
+            ) : null
           })}
+          <div className="Jarvis-res">
+          <div className="ChatPage-Output" ref={GsapOutput}>
+            {UserOutput.map((el, index) => {
+              return <p className="UserOutput">{el.output}</p>
+            })}
+          </div>
         </div>
-        <div className={`ChatPage-Search ${UserInput.length ===0?"centre":""}`} >
+        </div>
+        
+        <div className={`ChatPage-Search ${UserInput.length === 0 ? "centre" : ""}`} >
           <div className={`Search-button ${Expanded ? "Search-button-down" : ""}`}>
             <LuPlus size={25} className={Expanded ? "downside" : "upload-btn"} />
             <textarea className="input" ref={textarearef} onInput={handleInput}></textarea>
-            <button className={Expanded ? "downposition" : "default"} type="submit" onClick={handleSubmit}>
+            <button className={Expanded ? "downposition" : "default"} type="button" onClick={handleSubmit}>
               <LuArrowUp size={25} className="arrow" />
             </button>
           </div>
